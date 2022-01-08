ac8db4fa588f3268d6a5c218bd18ffd83aa5f457
VinayakGupta012 <89682227+VinayakGupta012@users.noreply.github.com>
2022-01-08 15:11:17 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "If 3√5 + √125 = 17.88 ,then what will be the value of √80 + 16√5?",
+    answer: "44.7",
+    options: [
+      "22.35",
+      "44.7",
+      "13.41",
+      "21.66"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Glass spheres (which submerge fully in water) of radius 7mm are dropped into a cylindrical vessel containingsome water. The diameter of the vessel is 14cm. Find how many spheres have been dropped in it if the water level rises by 3.5cm?",
+    answer: "375",
+    options: [
+      "375",
+      "300",
+      "37",
+      "Data Insufficient"
+    ]
+  },
+    {
+    numb: 3,
+    question: "There are 4 oranges, 5 apples and 6 mangoes in a basket. In how many ways can a person make a selection of fruits among the fruits in the basket?",
+    answer: "209",
+    options: [
+      "220",
+      "209",
+      "256",
+      "210"
+    ]
+  },
+    {
+    numb: 4,
+    question: "'A' sold an article for ₹8000 and incurred a loss. If he had sold the article for ₹9800, his gain would have been twice the amount of loss. At what price should the article be sold to earn 20% profit?",
+    answer: "₹10,320",
+    options: [
+      "₹10,840",
+      "₹9,820",
+      "₹10,320",
+      "₹9,840"
+    ]
+  },
+    {
+    numb: 5,
+    question: "After three successive equal percentage rise in the salary, the sum of 100 rupees turned into 140 rupees and 49 paise. Find the percentage rise in the salary?",
+    answer: "66%",
+    options: [
+      "12%",
+      "22%",
+      "66%",
+      "82%"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+     numb: 6,
+     question: "One side of a rectangle lies along the line 4x + 7y + 5 = 0. Two of its vertices are (-3, 1) and (1, 1). Which of the following may be an equation which represents any of the other three straight lines?",
+     answer: "7x - 4y = 3",
+     options: [
+       "7x - 4y = 3",
+       "7x - 4y + 3 = 0",
+       "y + 1 = 0",
+       "4x + 7y = 3"
+     ]
+   },
+/*   {
+   numb: 7,
+   question: "The sum of all the possible numbers of 4 digits formed by digits 3, 5, 5 and 6 using each digit once is:"
+   answer: "63327",
+   options: [
+     "64427",
+     "63327",
+     "65297",
+     "43521"
+   ]
+ },   */
+];
